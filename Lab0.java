bc6e1fc4a288ee78b9c2ae28dd0033a858ba94f4
andrewlsb <andrewlsb1119@gmail.com>
2025-07-19 05:41:08 -0700

    update

--- Lab0.java
+++ Lab0.java
@@ -1,5 +1,6 @@
 public class Lab0{
     public static void main(String[] args){
-        System.out.println("This is Andrew Lee");
+        System.out.println("CSC142-Intro to Java");
+        System.out.println("How java and other langauges differ for example how python and java is different")
     }
 }
